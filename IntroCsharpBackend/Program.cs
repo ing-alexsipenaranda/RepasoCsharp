ae211a713e5182c5a416608dac029c5e18382373
unknown <ingalexsipenaranda@gmail.com>
2026-03-25 07:39:37 -0500

    Lambda Expressions

--- IntroCsharpBackend/Program.cs
+++ IntroCsharpBackend/Program.cs
@@ -46,7 +46,7 @@
 //Some(sale);
 //Some(beer);
 
- 
+
 //void Some( ISave save)
 //{
 //    save.Save();
@@ -83,61 +83,145 @@
 //}
 
 
-var myList = new MyList<string>(10);
-var numbers = new MyList<int>(5);
-var beers  = new MyList<Beer>(3);
+//var myList = new MyList<string>(10);
+//var numbers = new MyList<int>(5);
+//var beers  = new MyList<Beer>(3);
 
-numbers.Add(1);
-numbers.Add(2);
-numbers.Add(3);
-numbers.Add(4);
-numbers.Add(5);
-numbers.Add(6);
-
-
-beers.Add(new Beer { Name = "Corona", Price = 4.50m });
-beers.Add(new Beer { Name = "Aguila", Price = 2.50m });
-beers.Add(new Beer { Name = "Poker", Price = 3.50m });
-beers.Add(new Beer { Name = "Budweiser", Price = 3.50m });
-
-Console.WriteLine(numbers.GetContent());
-Console.WriteLine(beers.GetContent());
+//numbers.Add(1);
+//numbers.Add(2);
+//numbers.Add(3);
+//numbers.Add(4);
+//numbers.Add(5);
+//numbers.Add(6);
 
 
-public class MyList<T>
-{
-    private List<T> _list;
-    private int _limit;
+//beers.Add(new Beer { Name = "Corona", Price = 4.50m });
+//beers.Add(new Beer { Name = "Aguila", Price = 2.50m });
+//beers.Add(new Beer { Name = "Poker", Price = 3.50m });
+//beers.Add(new Beer { Name = "Budweiser", Price = 3.50m });
 
-    public MyList(int limit)
-    {
-        _limit = limit;
-        _list = new List<T>();
-    }   
-    public void Add(T item)
-    {
-        if (_list.Count < _limit)
-        {
-            _list.Add(item);
-        }
-        else
-        {
-            Console.WriteLine("Limit reached");
-        }
-    }
-    public string GetContent()
-    {
-        return string.Join(", ", _list);
-    }
-}
+//Console.WriteLine(numbers.GetContent());
+//Console.WriteLine(beers.GetContent());
 
-public class Beer
-{
-    public string Name { get; set; }
-    public decimal Price { get; set; }
 
-    public override string ToString()
-    {
-        return $"{Name} ({Price:C})";
-    }
-}
+//public class MyList<T>
+//{
+//    private List<T> _list;
+//    private int _limit;
+
+//    public MyList(int limit)
+//    {
+//        _limit = limit;
+//        _list = new List<T>();
+//    }   
+//    public void Add(T item)
+//    {
+//        if (_list.Count < _limit)
+//        {
+//            _list.Add(item);
+//        }
+//        else
+//        {
+//            Console.WriteLine("Limit reached");
+//        }
+//    }
+//    public string GetContent()
+//    {
+//        return string.Join(", ", _list);
+//    }
+//}
+
+//public class Beer
+//{
+//    public string Name { get; set; }
+//    public decimal Price { get; set; }
+
+//    public override string ToString()
+//    {
+//        return $"{Name} ({Price:C})";
+//    }
+//}
+
+
+
+//SERIALIZACION Y DESERIALIZACION
+
+//using System.Text.Json;
+//var hector = new People { Name = "Hector", Age = 30 };
+
+//string json = JsonSerializer.Serialize(hector);
+//Console.WriteLine(json);
+
+//string MyJson = @"{
+//    ""Name"":""Hector"",
+//    ""Age"":30
+//}
+//";
+
+//People? juan = JsonSerializer.Deserialize<People>(MyJson); 
+//Console.WriteLine(juan?.Name);
+//Console.WriteLine(juan?.Age);
+//public class People
+//{
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+//}
+
+//FUNCIONES PURAS
+
+//int Sub(int a, int b)
+//{
+//    return a + b;
+//}
+//DateTime GetCurrentDate()
+//{
+//    return DateTime.Now;
+//}
+
+//Console.WriteLine(GetCurrentDate());
+
+
+//var beer1 = new Beer { Name = "Corona" };
+
+//Beer ToUpper(Beer beer1)
+//{
+//    var beer2 = new Beer { Name =
+//            beer1.Name.ToUpper() };
+//    return beer2;
+//}
+
+//Console.WriteLine(beer1.Name);
+//Console.WriteLine(ToUpper(beer1).Name);
+//class Beer
+//{
+//    public string Name { get; set; }
+//}
+
+//var show = Show2;
+//Someday(show, "algun dia");
+
+//void Show()
+//{
+//    Console.WriteLine("Hello World"); 
+//}
+//string Show2(string message)
+//{
+//    return message;
+//}
+//show("hola");
+
+//void Some (Action<string> action, string message)
+//{
+//    action("Hello from Some");
+//    Console.WriteLine(message);
+//}
+
+//void Someday(Func<string, string> fn,string message)
+//{
+//    Console.WriteLine("Hello from Some");
+//    Console.WriteLine(fn(message));
+//}
+
+//LAMBDA EXPRESSIONS
+
+
